d4213386b9558ff20267c6d4877ee70493e1dc04
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2025-01-28 23:32:39 +0530

    Revert "schema,swagger,entities created"
    This reverts commit b7420679d6a3ad76f4a05f0c056784d96f8f79e9.

--- src/config/database.config.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize("Aahar-db", "postgres", "0508", {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
-
-export default sequelize;
