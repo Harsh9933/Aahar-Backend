508f0517e5f9a2d563ed1d6365be0f4d1f983a3a
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2025-01-30 00:18:05 +0530

    env

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,27 +1,32 @@
 import express from "express";
+import dotenv from "dotenv"; 
 import sequelize from "./config/database.config.js";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import swaggerOptions from "./utils/swagger.js";
 import RouteHandler from "./routehandler/v1/handler.js";
 
+dotenv.config(); 
+
 const app = express();
 
 app.use(express.json());
+
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 RouteHandler(app);
-app.get("/",(req,res)=>{
-res.send({message:"hello world!"})
-})
-const PORT = 5000;
 
+app.get("/", (req, res) => {
+  res.send({ message: "hello world!" });
+});
 
+const PORT = process.env.PORT || 5000; 
 (async () => {
   try {
     await sequelize.authenticate();
     console.log("Database connected!");
-    await sequelize.sync(); 
+    await sequelize.sync();
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
--- backend/src/config/database.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sequelize_1 = require("sequelize");
-var sequelize = new sequelize_1.Sequelize("Aahar-db", "postgres", "0508", {
-    host: "localhost",
-    dialect: "postgres",
-    logging: false,
-});
-exports.default = sequelize;
